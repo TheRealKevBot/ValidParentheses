2306d5541d57ced197a49ded99aa55aa64027603
TheRealKevBot <Kglas001@gmail.com>
2021-06-16 13:56:47 -0600

    First

---
+++ index.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
+// The function should return true if the string is valid, and false if it's invalid.
+
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
+function validParentheses(parens){
+    //TODO 
+} 
+
+console.log(validParentheses( "()" ), true);
+console.log(validParentheses( "())" ), false);
