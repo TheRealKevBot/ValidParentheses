f13f1b0f3207c37985eb60890f2eda86714dd027
TheRealKevBot <Kglas001@gmail.com>
2021-06-16 16:41:04 -0600

    Build

--- index.js
+++ index.js
@@ -7,8 +7,21 @@
 // "(())((()())())"  =>  true
 
 function validParentheses(parens){
-    //TODO 
+    let answer = true
+    let count = []
+    let arr = parens.split('')
+    arr.forEach(par => {
+        par === '(' ? count.push('Open') : null
+        !count.includes('Open') && par === ')' ? count.push('Close') : null
+        par === ')' && count.includes('Open') ? count.splice(arr.indexOf('Open'),1) : null
+    })
+    parens[0] ===')' ? answer = false : null
+    count.length > 0 ? answer = false : null
+    return answer 
 } 
 
 console.log(validParentheses( "()" ), true);
+console.log(validParentheses( "())" ), false);
+console.log(validParentheses( "()))))()" ), false);
+console.log(validParentheses( "())(()(())" ), false);
 console.log(validParentheses( "())" ), false);
